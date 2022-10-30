2993d6cc80e44742d76c0241921db0eb96a092c8
Billy Chen <billychen0894@gmail.com>
2022-10-30 10:57:06 +0800

    init bootstrap, restaurant.json

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
-const express = require('express')
-const app = express()
-const port = 3000
-const exphbs = require('express-handlebars')
+const express = require("express");
+const app = express();
+const port = 3000;
+const exphbs = require("express-handlebars");
 
-app.engine('handlebars', exphbs.engine({ defaultLayout: 'main' }))
-app.set('view engine', 'handlebars')
+// setting template engine
+app.engine("handlebars", exphbs.engine({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
-app.get('/', (req, res) => {
-  res.send('work?')
-})
+// setting static files
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
 
 app.listen(port, () => {
-  console.log(`Listening to http://localhost${port}`)
-})
+  console.log(`Listening to http://localhost${port}`);
+});
